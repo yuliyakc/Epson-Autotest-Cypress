2380ca179336eca12268501a2ca329fbc69b86c5
Mariya <compmery@gmail.com>
2021-11-12 08:03:31 +0200

    small packages changes

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,11 +16,19 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 require('cypress-xpath')
+// Add Screenshot to Mochawesome Report
 import addContext from "mochawesome/addContext";
+
+Cypress.on("uncaught:exception", (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false;
+});
+
 Cypress.on("test:after:run", (test, runnable) => {
     if (test.state === "failed") {
         const screenshot = `assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
-        addContext({ test }, screenshot);
+        addContext({test}, screenshot);
     }
 });
 
